e3354bb17f2ad0c1042a2a930424d1c1a97dd0d6
maransi <maransi>
2022-02-02 17:41:26 -0300

    Aula 18.6 a 8

--- src/app/lancamentos/lancamento.service.ts
+++ src/app/lancamentos/lancamento.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { Lancamento } from 'app/domain/lancamento';
+// import { Lancamento } from 'app/domain/lancamento';
+import {Lancamento} from './../core/model';
 import { URLSearchParams} from '@angular/http';
 import * as moment from 'moment';
 
@@ -95,6 +96,54 @@ export class LancamentoService {
                             { headers })
                     .toPromise()
                     .then( response => response.json() );
+  }
+
+  atualizar(lancamento: Lancamento) : Promise<Lancamento>{
+    const headers = new Headers();
+
+    headers.append("Authorization","Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==");
+    headers.append("Content-Type", "application/json");
+
+    return this.http.put( `${this.lancamentoUrl}/${lancamento.codigo}`, 
+                          JSON.stringify( lancamento), 
+                          {headers})
+                      .toPromise()
+                      .then( response => {
+                          const lancamentoAlterado = response.json() as Lancamento;
+
+                          this.converterStringsParaDatas([lancamentoAlterado]);
+
+                          return lancamentoAlterado;
+                      });
+  }
+
+  buscarPorCodigo(codigo: number): Promise<Lancamento>{
+    const headers = new Headers();
+
+    headers.append("Authorization", "Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==");
+    headers.append("Content-Type","application/json");
+
+    return this.http.get( `${this.lancamentoUrl}/${codigo}`,{ headers } )
+              .toPromise()
+              .then( response => {
+                const lancamento = response.json() as Lancamento;
+
+                this.converterStringsParaDatas( [lancamento]);
+
+                return lancamento;
+              });
+  }
+
+
+  converterStringsParaDatas( lancamentos: Lancamento[] ){
+    for (const lancamento of lancamentos ){
+      lancamento.dataVencimento = moment( lancamento.dataVencimento, 'YYYY-MM-DD').toDate();
+
+      if (lancamento.dataPagamento){
+        lancamento.dataPagamento = moment( lancamento.dataPagamento, 'YYYY-MM-DD').toDate();
+      }
+    }
+
   }
 
 }
